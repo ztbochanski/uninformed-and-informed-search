b5247cf2ecd2797b0ef6a00d49d39f87acaac45f
zach <ztbochanski@gmail.com>
2022-04-17 15:38:57 -0700

    add actions to problem

--- problem.py
+++ problem.py
@@ -6,11 +6,15 @@ class Problem:
 
     Goal state:
         the desired outcome of the problem
+
+    Actions:
+        available actions to the problem
     """
 
-    def __init__(self, initial_state, goal_state):
+    def __init__(self, initial_state, goal_state, actions):
         self.initial_state = initial_state
         self.goal_state = goal_state
+        self.actions = actions
 
     def get_initial_state(self):
         return self.initial_state
@@ -18,18 +22,18 @@ class Problem:
     def get_goal_state(self):
         return self.goal_state
 
+    def get_actions(self):
+        return self.actions
+
     def goal_test(self, state):
         return self.goal_state == state
-
-    def do_action(self, state):
-        return f"fake action state: {state}"
 
 
 def main():
     """test problem
     """
 
-    problem = Problem("fake initial", "fake goal")
+    problem = Problem("fake initial", "fake goal", "fake actions")
     if problem.goal_test("fake goal"):
         print("passed")
     else:
