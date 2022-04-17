f6ffc95fe2eb4cf16b4da63ea1b28dd1a43ff17e
zach <ztbochanski@gmail.com>
2022-04-17 12:01:55 -0700

    add tests to available actions

--- actions.py
+++ actions.py
@@ -6,10 +6,22 @@ class Actions:
     """
 
     def __init__(self):
-        self.actions = {
+        self.available = {
             "action1": Action(passengers=["chicken"], count=1),
             "action2": Action(passengers=["chicken"], count=2),
             "action3": Action(passengers=["wolf"], count=1),
             "action4": Action(passengers=["chicken", "wolf"], count=1),
             "action5": Action(passengers=["wolf"], count=2)
         }
+
+
+def main():
+    """test calling actions
+    """
+    state = (0, 0, 0,  # <- LEFT RIVER BANK
+             3, 3, 1)  # <- RIGHT RIVER BANK
+    print(Actions().available["action1"].do(state))
+
+
+if __name__ == "__main__":
+    main()
